408b9900fa77c09c6476b54ec89bc357f170c352
Anton Mindlin <anton@MacBook-Pro-Anton.local>
2022-06-04 21:41:09 +0200

    Lesson 6 ready 2

--- messenger/decos.py
+++ messenger/decos.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import traceback
 
 from log import client_log_config, server_log_config
 
@@ -15,7 +16,8 @@ def log(func):
     def wrapper(*args, **kwargs):
         res = func(*args, **kwargs)
         LOGGER.debug(f'Function {func.__name__} was called with parameters {args, kwargs}. '
-                     f'From module {func.__module__}.')
+                     f'From module {func.__module__}. '
+                     f'Inside function {traceback.format_stack()[0].strip().split()[-1]}')
         return res
 
     return wrapper
